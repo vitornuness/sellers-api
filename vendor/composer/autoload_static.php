8736fd005d093eb0098daec6e1fc2903f79c543b
vitornuness <vitor1nuness@gmail.com>
2025-01-04 18:12:05 -0300

    chore: alterando composer e adicionando ao gitignore

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,28 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit4af2990918fd4f1ab1f5da3817dfa0d4
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/core',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +35,8 @@ class ComposerStaticInit4af2990918fd4f1ab1f5da3817dfa0d4
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4af2990918fd4f1ab1f5da3817dfa0d4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4af2990918fd4f1ab1f5da3817dfa0d4::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit4af2990918fd4f1ab1f5da3817dfa0d4::$classMap;
 
         }, null, ClassLoader::class);
